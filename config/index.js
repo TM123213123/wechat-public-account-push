238bf9bcba7e98beade4a7a0a0934cdbce175cfd
TM123213123 <113531736+TM123213123@users.noreply.github.com>
2022-09-18 00:15:37 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -11,7 +11,7 @@ export const config = {
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "c2b137dac640ce6fff1fdc4cf47f5227",
+  APP_SECRET: "5eb2eff9a1524cbc9bdab29c1da8cbd1",
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
